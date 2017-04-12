6f24d923c593c083ac823c172c020cc9a4ee3b3f
magol <magnusosterlund78@gmail.com>
2017-04-12 13:17:09 +0200

    Add project files.

---
+++ Foo.Library/MyLibrary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Foo.Library
+{
+    public class MyLibrary
+    {
+        public static string Format(FormattableString formattable) => formattable.ToString();
+
+        public static void PrintLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ FooConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Foo.Library;
+
+namespace FooConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string hello = "Hello";
+            const string world = "World";
+
+            string text = MyLibrary.Format($"{hello} {world}!");
+            MyLibrary.PrintLine(text);
+
+            Console.ReadKey();
+        }
+    }
+}
